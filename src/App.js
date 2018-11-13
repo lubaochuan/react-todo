94df3a7a875622f8698409c04ea07e0714bd4dc3
Baochuan Lu <blu@sbuniv.edu>
2018-11-13 13:13:34 +0000

    add todo item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoList from './TodoList'
+import TodoItems from './TodoItems'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+
+  inputElement = React.createRef()
+
+  handleInput = event => {
+    this.setState({
+      currentItem: {text:event.target.value, key: Date.now()}
+    })
+  }
+
+  addItem = event => {
+    event.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== ''){
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <TodoList
+          addItem={this.addItem}
+          inputElement={this.inputElement}
+          handleInput={this.handleInput}
+          currentItem={this.state.currentItem} />
+        <TodoItems entries={this.state.items} />
       </div>
     )
   }
